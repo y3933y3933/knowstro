9c62c31038334cc6556ea00958c4c0197c035e7a
Joanne Lin <y3933y3933@gmail.com>
2025-04-28 22:53:43 +0800

    fix: change error response JSON to abortWithJSON

--- internal/response/message.go
+++ internal/response/message.go
@@ -7,10 +7,11 @@ import (
 )
 
 const (
-	MsgRecordNotFound      = "record not found"
-	MsgInternalServerError = "internal server error"
-	MsgFailedValidation    = "validation fail"
-	MsgInvalidCredentials  = "invalid authentication credentials"
+	MsgRecordNotFound             = "record not found"
+	MsgInternalServerError        = "internal server error"
+	MsgFailedValidation           = "validation fail"
+	MsgInvalidCredentials         = "invalid authentication credentials"
+	MsgInvalidAuthenticationToken = "invalid or missing authentication token"
 )
 
 func SuccessOK(c *gin.Context, data any) {
@@ -25,30 +26,36 @@ func SuccessCreated(c *gin.Context, data any) {
 
 func RecordNotFound(c *gin.Context) {
 	status, res := NewError(http.StatusNotFound, MsgRecordNotFound)
-	c.JSON(status, res)
+	c.AbortWithStatusJSON(status, res)
 }
 
 func InternalError(c *gin.Context) {
 	status, res := NewError(http.StatusInternalServerError, MsgInternalServerError)
-	c.JSON(status, res)
+	c.AbortWithStatusJSON(status, res)
 }
 
 func BadRequest(c *gin.Context, msg string) {
 	status, res := NewError(http.StatusBadRequest, msg)
-	c.JSON(status, res)
+	c.AbortWithStatusJSON(status, res)
 }
 
 func UnprocessableError(c *gin.Context, msg string) {
 	status, res := NewError(http.StatusUnprocessableEntity, msg)
-	c.JSON(status, res)
+	c.AbortWithStatusJSON(status, res)
 }
 
 func FailedValidationError(c *gin.Context, details []FieldError) {
 	status, res := NewError(http.StatusUnprocessableEntity, MsgFailedValidation, details...)
-	c.JSON(status, res)
+	c.AbortWithStatusJSON(status, res)
 }
 
 func InvalidCredential(c *gin.Context) {
 	status, res := NewError(http.StatusUnauthorized, MsgInvalidCredentials)
-	c.JSON(status, res)
+	c.AbortWithStatusJSON(status, res)
+}
+
+func InvalidAuthenticationToken(c *gin.Context) {
+	c.Header("WWW-Authenticate", "Bearer")
+	status, res := NewError(http.StatusUnauthorized, MsgInvalidAuthenticationToken)
+	c.AbortWithStatusJSON(status, res)
 }
